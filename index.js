4178d11436adb1246c3bc45102e8703d2d4d959f
ayoungdrew <andrewyoung504@gmail.com>
2018-04-09 11:08:24 -0400

    Fixes
    Fixes

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 $(() => {
-  $(".test-text, .portfolio-tile").hover(function() {
-    $(this).addClass('hvr-sweep-to-top');
-  });
-  $(".test-text, .portfolio-tile").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function() {
-    $(this).removeClass('hvr-sweep-to-top');
-  });
-  $("#about-link").click(function() {
-      $('html, body').animate({
-          scrollTop: $("#about").offset().top
-      }, 2000);
-  });
+
 })
