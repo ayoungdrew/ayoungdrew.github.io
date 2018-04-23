705921bf03b51688915879dc1192fff15a3d4de3
ayoungdrew <andrewyoung504@gmail.com>
2018-04-23 03:06:35 -0400

    Refactored jquery
    Refactored jquery

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// $(() => {
-//     let scrollLink = $('.scroll')
-//     scrollLink.click(function(e) {
-//       e.preventDefault();
-//       $('body,html').animate({
-//         scrollTop: $(this.hash).offset().top
-//     }, 600 )
-//     })
-// })
+$(() => {
+    let scrollLink = $('.scroll')
+    scrollLink.click(function(e) {
+      e.preventDefault();
+      $('body,html').animate({
+        scrollTop: $(this.hash).offset().top
+    }, 600 )
+    })
+})
