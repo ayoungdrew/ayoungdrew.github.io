8a44a1c0ba0aaea04dec091a81c03be11208db52
ayoungdrew <andrewyoung504@gmail.com>
2018-04-23 02:28:38 -0400

    Smooth scroll added
    Smooth scroll added

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 $(() => {
-
+    let scrollLink = $('.scroll')
+    scrollLink.click(function(e) {
+      e.preventDefault();
+      $('body,html').animate({
+      //
+        scrollTop: $(this.hash).offset().top
+      //
+    }, 600 )
+    })
+    // $("#about").click(function(event) {
+    //   event.preventDefault();
+    //   $("html, body").animate({
+    //     scrollTop: $($(this).attr("href")).offset().top
+    //   }, 500);
 })
